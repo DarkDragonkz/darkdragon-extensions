a123e6b67a0e17c936097c399d79c00090037aeb
DarkDragonkz <kirolosmarco2013@gmail.com>
2025-11-20 14:43:36 +0000

    Deploying to gh-pages from @ DarkDragonkz/extensions-foreign@6f226f7685c86cafcacd8ba43f04079621c55ee7 🚀

--- 0.8/MangaWorld/index.js
+++ 0.8/MangaWorld/index.js
@@ -466,7 +466,7 @@ const parser_1 = require("./parser");
 const helper_1 = require("./helper");
 const MW_DOMAIN = 'https://www.mangaworld.mx';
 exports.MangaWorldInfo = {
-    version: '3.0.2',
+    version: '3.0.3',
     name: 'MangaWorld',
     description: 'Extension that pulls manga from MangaWorld (0.8).',
     author: 'NmN',
@@ -484,6 +484,7 @@ exports.MangaWorldInfo = {
     intents: types_1.SourceIntents.MANGA_CHAPTERS | types_1.SourceIntents.HOMEPAGE_SECTIONS | types_1.SourceIntents.CLOUDFLARE_BYPASS_REQUIRED,
 };
 class MangaWorld {
+    // MODIFICA: Usiamo 'any' per evitare errori di importazione di CheerioAPI
     constructor(cheerio) {
         this.cheerio = cheerio;
         this.baseUrl = MW_DOMAIN;
