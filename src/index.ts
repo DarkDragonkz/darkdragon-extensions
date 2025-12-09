8a3357ba91473f84cd9fcac17eff86fdc770e269
DarkDragonkz <kirolosmarco2013@gmail.com>
2025-12-09 18:53:52 +0100

    up

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { WeebCentral, WeebCentralInfo } from './WeebCentral/WeebCentral'
 import { MangaDex, MangaDexInfo } from './MangaDex/MangaDex'
 import { ReadAllComics, ReadAllComicsInfo } from './ReadAllComics/ReadAllComics'
 import { MangaParkIT, MangaParkITInfo } from './MangaParkIT/MangaParkIT'
-
 import * as cheerio from 'cheerio'
 
 export const MangaWorldSource = new MangaWorld(cheerio)
